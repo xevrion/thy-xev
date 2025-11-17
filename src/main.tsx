9ebaaa1b56d5388486bab9dcb93f04f70182eb02
xevrion <krbavadiya11@gmail.com>
2025-11-17 14:49:03 +0530

    HeadProvider issue fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { createRoot } from 'react-dom/client'
 import './styles/app.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import { HeadProvider } from 'react-head'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <HeadProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </HeadProvider>,
 )
