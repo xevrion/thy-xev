ae60330320ea16d607ac36be96ecb46390996086
xevrion <krbavadiya11@gmail.com>
2025-12-24 16:05:35 +0530

    fix: type module error

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import type { Request, Response } from "express";
 import axios from "axios";
 import dotenv from "dotenv";
 import querystring from "querystring";
